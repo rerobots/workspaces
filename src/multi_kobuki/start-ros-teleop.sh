7b9814c55d8592d1e034f15e6889e19f43f36a4a
Scott C. Livingston <scott@rerobots.net>
2020-08-22 17:27:24 -0700

    multi_kobuki: improve simple start script

--- src/multi_kobuki/start-ros-teleop.sh
+++ src/multi_kobuki/start-ros-teleop.sh
@@ -3,8 +3,28 @@
 # Copyright (C) 2020 rerobots, Inc.
 
 source /opt/ros/melodic/setup.bash
+if pgrep -f robot.launch > /dev/null && rostopic list > /dev/null 2>&1; then
+    echo 'WARNING: robot.launch appears to be running'
+    echo 'WARNING: you may need to stop it first, for example'
+    echo ''
+    echo 'tmux attach'
+    echo ''
+    echo 'then Ctrl-C, or pkill -f robot.launch'
+    sleep 2
+fi
+echo 'Starting'
 if test -n "$TMUX"; then
-    exec roslaunch robot.launch
+    tmux new-window -d roslaunch robot.launch
+    while ! rostopic list > /dev/null 2>&1; do
+	sleep 2
+	echo -n '.'
+    done
+    exec roslaunch kobuki_keyop keyop.launch
 else
-    exec tmux new-session roslaunch robot.launch
+    tmux new-session -d roslaunch robot.launch
+    while ! rostopic list > /dev/null 2>&1; do
+	sleep 2
+	echo -n '.'
+    done
+    exec tmux new-session roslaunch kobuki_keyop keyop.launch
 fi
